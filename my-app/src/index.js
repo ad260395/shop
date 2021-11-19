305ddd44e59f60bd78cd12c5868046d096d9403a
Ashish Deshpande <51477456+ad260395@users.noreply.github.com>
2021-11-19 14:25:11 +0530

    Code formatted

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './bootstrap.min.css';
-import './index.css';
-import App from './App';
-import store from './store';
-import { Provider } from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./bootstrap.min.css";
+import "./index.css";
+import App from "./App";
+import store from "./store";
+import { Provider } from "react-redux";
 
 ReactDOM.render(
-	<Provider store={store}>
-		<App />
-	</Provider>,
-	document.getElementById('root')
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
