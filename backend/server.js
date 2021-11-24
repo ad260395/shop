cf2e9aa3c0725cdda3baeb434c60160b23532317
Ashish Deshpande <51477456+ad260395@users.noreply.github.com>
2021-11-24 09:33:48 +0530

    backens user create and authenticate

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
-import express from 'express';
-import productRoutes from './routes/productRoutes.js';
-import dotenv from 'dotenv';
-import connectDB from './config/db.js';
-import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import express from "express";
+import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
 connectDB();
@@ -11,17 +12,20 @@ const app = new express();
 
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-	res.send('API is running');
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API is running");
 });
-app.use('/api/products', productRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 app.use(notFound);
 
 app.use(errorHandler);
 
 app.listen(PORT, () => {
-	console.log(
-		`Server running in ${process.env.NODE_ENV} mode started on port ${PORT}`
-	);
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode started on port ${PORT}`
+  );
 });
